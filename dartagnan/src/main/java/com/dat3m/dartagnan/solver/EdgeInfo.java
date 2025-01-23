bd15370b554b795dbe48788106a1189881853029
Natalia <n.gacek@tu-bs.de>
2025-01-23 18:33:46 +0100

    prepare unfolding

--- dartagnan/src/main/java/com/dat3m/dartagnan/solver/EdgeInfo.java
+++
@@ -1,7 +0,0 @@
-package com.dat3m.dartagnan.solver;
-
-import com.dat3m.dartagnan.program.event.Event;
-import com.dat3m.dartagnan.wmm.Relation;
-
-public record EdgeInfo(Relation relation, Event source, Event target) {
-}
